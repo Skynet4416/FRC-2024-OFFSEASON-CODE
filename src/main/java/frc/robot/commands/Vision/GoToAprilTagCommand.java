963bcf7943372b19f597fabb1e0ea775b3b2d516
bobthebuilder98765 <124575412+bobthebuilder98765@users.noreply.github.com>
2024-12-29 17:03:13 +0200

    moved setpoint to initialize
    moved setpoint to initialize

--- src/main/java/frc/robot/commands/Vision/GoToAprilTagCommand.java
+++ src/main/java/frc/robot/commands/Vision/GoToAprilTagCommand.java
@@ -32,6 +32,8 @@ public class GoToAprilTagCommand extends Command {
     public void initialize() {
         this.targetVisible = false;
         this.botPose = new double[6];
+        this.driveSubsystem.yawRotationPIDSetPoint(0);
+
     }
  
     // Called every time the scheduler runs while the command is scheduled.
@@ -46,7 +48,6 @@ public class GoToAprilTagCommand extends Command {
             // double speedX =  distanceX * 0.1;
             // double speedY = distanceY * 0.1;
             
-            this.driveSubsystem.yawRotationPIDSetPoint(yawRotationNeededInDegrees);
             driveSubsystem.setModules(x,y,this.driveSubsystem.calculateYawRotationInPID(yawRotationNeededInDegrees), this.speedmodSupplier.getAsDouble());
         }
     }
